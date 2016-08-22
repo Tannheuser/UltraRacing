41c3f97b4b10f563e0ed0f7160d7bf70f065624b
tannheuser <tannheuser@localhost.localdomain>
2016-08-22 19:48:58 +0300

    Implement cource part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'Ultra Racing Schedule';
-    race = {
-        id: 1,
-        name: 'Daytona Thunderdome',
-        date: new Date('2512-01-04T14:00:00'),
-        about: 'Race through the ruins of an ancient Florida battle arena.',
-        entryFee: 3200
+    races = [{
+        "id": 1,
+        "name": "Daytona Thunderdome",
+        "date": new Date('2512-01-04T14:00:00'),
+        "about": "Race through the ruins of an ancient Florida battle arena.",
+        "entryFee": 3200,
+        "isRacing": false
+    }, {
+        "id": 2,
+        "name": "San Francisco Ruins",
+        "date": new Date('2512-07-03T20:00:00'),
+        "about": "Drift down the streets of a city almost sunk under the ocean.",
+        "entryFee": 4700,
+        "isRacing": true
+    }, {
+        "id": 3,
+        "name": "New York City Skyline",
+        "date": new Date('2512-07-12T21:00:00'),
+        "about": "Fly between buildings in the electronic sky.",
+        "entryFee": 4300,
+        "isRacing": true
+    }];
+
+    totalCost() {
+        return this.races.reduce((prev, current) => prev + (current.isRacing ? current.entryFee : 0), 0);
     };
+    // totalCarParts() {
+    //     return this.carParts.reduce((prev, current) => prev + current.inStock, 0);
+    //
+    //     let sum = 0;
+    //
+    //     for(let carPart of this.carParts) {
+    //         sum += carPart.inStock;
+    //     }
+    //
+    //     return sum;
+    // }
 }
