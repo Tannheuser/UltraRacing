b0d8c76a5b53e975031a5a48022d5b881a6293e1
Tannheuser <aleksey.ryabinin@gmail.com>
2017-06-05 19:51:23 +0300

    Implement races list

--- src/app/ur-root.component.ts
+++ src/app/ur-root.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { MenuItem } from './model/menuItem';
 
 @Component({
-  selector: 'racing-root',
-  templateUrl: './racing-root.component.html',
-  styleUrls: ['./racing-root.component.scss']
+  selector: 'ur-root',
+  templateUrl: './ur-root.component.html',
+  styleUrls: ['./ur-root.component.scss']
 })
 export class RootComponent {
   title = 'Ultra Racing';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { RacingModule } from './app/racing-root.module';
+import { RacingModule } from './app/ur-root.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
