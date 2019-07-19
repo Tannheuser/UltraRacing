424502298803ebe2de71162dbe65f9f8ffbd90d2
Tannheuser <aleksey.ryabinin@gmail.com>
2019-07-19 10:07:40 +0200

    Update car model. Add cars to mock list.

--- src/app/cars/model/car.ts
+++ src/app/cars/model/car.ts
@@ -4,15 +4,13 @@ import { Transmission } from '@app/cars/model/transmission';
 export interface Car extends BaseEntity {
   acceleration: number;
   breaking: number;
-  cornering: number;
+  handling: number;
   engineCapacity: number;
   manufacturer: BaseEntity;
   mileage: number;
   maxSpeed: number;
   maxPower: number;
-  maxTorque: number;
   stability: number;
   transmission: Transmission;
   year: number;
-  weight: number;
 }
--- src/app/mock/cars.ts
+++ src/app/mock/cars.ts
@@ -1,22 +1,79 @@
-import { Car, Transmission } from '@app/cars/model';
-import { BaseEntity } from '@app/core/model';
+import { Car, DriveTrainType, TransmissionType } from '@app/cars/model';
 
 export const Cars: Car[] = [
-  // {
-  //   id: 1,
-  //   name: 'A100',
-  //   acceleration: number;
-  //   breaking: number;
-  //   cornering: number;
-  //   engineCapacity:
-  //   manufacturer: BaseEntity;
-  //   mileage: number;
-  //   maxSpeed: number;
-  //   maxPower: number;
-  //   maxTorque: number;
-  //   stability: number;
-  //   transmission: Transmission;
-  //   year: number;
-  //   weight: number;
-  // }
+  {
+    id: 1,
+    name: 'A100',
+    acceleration: 0.3,
+    breaking: 0.2,
+    handling: 0.3,
+    engineCapacity: 2.2,
+    manufacturer: { id: 1, name: 'Audi'},
+    mileage: 189435,
+    maxSpeed: 220,
+    maxPower: 162,
+    stability: 0.3,
+    transmission: { type: TransmissionType.Manual, driveTrain: DriveTrainType.Front, speeds: 5 },
+    year: 1991
+  },
+  {
+    id: 2,
+    name: 'Corolla AE (86)',
+    acceleration: 0.2,
+    breaking: 0.2,
+    handling: 0.3,
+    engineCapacity: 1.6,
+    manufacturer: { id: 2, name: 'Toyota'},
+    mileage: 212012,
+    maxSpeed: 180,
+    maxPower: 120,
+    stability: 0.3,
+    transmission: { type: TransmissionType.Manual, driveTrain: DriveTrainType.Rear, speeds: 5 },
+    year: 1987
+  },
+  {
+    id: 3,
+    name: 'Golf II GTI',
+    acceleration: 0.3,
+    breaking: 0.3,
+    handling: 0.3,
+    engineCapacity: 1.8,
+    manufacturer: { id: 3, name: 'Volkswagen'},
+    mileage: 205403,
+    maxSpeed: 200,
+    maxPower: 139,
+    stability: 0.3,
+    transmission: { type: TransmissionType.Manual, driveTrain: DriveTrainType.Front, speeds: 5 },
+    year: 1987
+  },
+  {
+    id: 4,
+    name: 'M3',
+    acceleration: 0.3,
+    breaking: 0.3,
+    handling: 0.3,
+    engineCapacity: 1.8,
+    manufacturer: { id: 4, name: 'BMW'},
+    mileage: 193400,
+    maxSpeed: 235,
+    maxPower: 200,
+    stability: 0.3,
+    transmission: { type: TransmissionType.Manual, driveTrain: DriveTrainType.Front, speeds: 5 },
+    year: 1988
+  },
+  {
+    id: 5,
+    name: 'RX7',
+    acceleration: 0.3,
+    breaking: 0.3,
+    handling: 0.3,
+    engineCapacity: 1.3,
+    manufacturer: { id: 5, name: 'Mazda'},
+    mileage: 235600,
+    maxSpeed: 220,
+    maxPower: 185,
+    stability: 0.3,
+    transmission: { type: TransmissionType.Manual, driveTrain: DriveTrainType.Rear, speeds: 5 },
+    year: 1988
+  }
 ];
